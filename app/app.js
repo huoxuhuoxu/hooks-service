bc2dff2e58d5ad648822e9b2ee2eb37df94fce5d
huzhen <zhen.hu@evenhidata.com>
2018-05-15 01:04:23 +0800

    ..

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ const logger = require("koa-logger");
 const app = new Koa();
 const router = new Router();
 
+const hooks = require("./controllers/hooks");
+
 app.use(logger((str, args) => {
     args.splice(2, 0, new Date());
     console.log(...args);
@@ -15,14 +17,11 @@ app.use(logger((str, args) => {
 app.use(bodyParser());
 
 
-router.get("/api/post-receive", async ctx => {
-    ctx.body = "1111";
-});
+// routes
+router.post("/api/post-receive", hooks.post_receive);
 
 
 app.use(router.routes()).use(router.allowedMethods());
 
 module.exports = app;
-
-
 
---
+++ app/controllers/hooks.js
@@ -0,0 +1,12 @@
+
+/**
+ *  @readme
+ *      处理由git仓库发起的hooks
+ * 
+ */
+
+
+module.exports.post_receive = async ctx => {
+    ctx.body = "1111";
+};
+
