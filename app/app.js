75520170868cfd2b87e32f4a849f2bda73d67806
huzhen <zhen.hu@evenhidata.com>
2018-05-15 00:54:29 +0800

    ..

--- app/app.js
+++ app/app.js
@@ -1,6 +1,28 @@
+// app
+
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const logger = require("koa-logger");
+
+const app = new Koa();
+const router = new Router();
+
+app.use(logger((str, args) => {
+    args.splice(2, 0, new Date());
+    console.log(...args);
+}));
+app.use(bodyParser());
 
 
+router.get("/api/post-receive", async ctx => {
+    ctx.body = "1111";
+});
 
+
+app.use(router.routes()).use(router.allowedMethods());
+
+module.exports = app;
 
 
 
---
+++ index.js
@@ -0,0 +1,10 @@
+
+require("dotenv").config();
+const app = require("./app/app");
+const PORT = process.env.PORT;
+
+
+
+app.listen(PORT);
+
+console.log("[info] listen port: %s", PORT);
