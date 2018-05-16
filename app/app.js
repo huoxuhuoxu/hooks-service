2c65825deac259d660787324ce9022d9b5480781
huzhen <zhen.hu@evenhidata.com>
2018-05-17 00:07:01 +0800

    添加静态代理

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
 // app
 
+const path = require("path");
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const logger = require("koa-logger");
+const static = require("koa-static");
 
 const app = new Koa();
 const router = new Router();
 
 const hooks = require("./controllers/hooks");
+
 
 app.use(logger((str, args) => {
     args.splice(2, 0, new Date());
@@ -17,8 +20,25 @@ app.use(logger((str, args) => {
 app.use(bodyParser());
 
 
+// web page
+app.use(static(path.join(__dirname, "../public")));
+
+// middleware
+app.use(async (ctx, next) => {
+    ctx.response.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    });
+    next();
+});
+
+
 // routes
 router.post("/api/post-receive", hooks.post_receive);
+router.put("/testing", async (ctx) => {
+    ctx.body = "111";
+});
 
 
 app.use(router.routes()).use(router.allowedMethods());
