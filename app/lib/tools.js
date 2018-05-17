de1b2cf8baa9da73021eeda826a320fc18d8e7bd
huzhen <zhen.hu@evenhidata.com>
2018-05-17 10:30:52 +0800

    ..

--- app/lib/tools.js
+++ app/lib/tools.js
@@ -2,9 +2,9 @@
 const execSync = require("child_process").execSync;
 
 module.exports.execSync = (...args) => {
-    console.log("[info] 需要执行的命令: %s", args.join(" , "));
+    console.log("[info] 需要执行的命令: \r\n%s", args.join(" , "));
     const resul = execSync(...args).toString();
-    console.log("[info] 执行结果: %s", resul);
+    console.log("[info] 执行结果: \r\n%s", resul);
     return resul;
 };
 
